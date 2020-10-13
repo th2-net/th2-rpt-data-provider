1aa8f78b9fb3af9f3ce019ebce31db09a45b0ddc
anatoly.ivanov <anatoly.ivanov@exactprosystems.com>
2020-10-13 14:30:46 +0300

    TH2-800 - amqp connection fix

--- src/main/kotlin/com/exactpro/th2/reportdataprovider/services/RabbitMqService.kt
+++ src/main/kotlin/com/exactpro/th2/reportdataprovider/services/RabbitMqService.kt
@@ -31,77 +31,82 @@ import mu.KotlinLogging
 class RabbitMqService(private val configuration: Configuration) {
 
     companion object {
-        val factory = ConnectionFactory()
         val logger = KotlinLogging.logger { }
     }
 
     private val decodeRequests = HashSet<Pair<MessageID, Channel<Message>>>()
 
-    private val amqpUri =
-        "amqp://${configuration.amqpUsername.value}:${configuration.amqpPassword.value}@${configuration.amqpHost.value}:${configuration.amqpPort.value}/${configuration.amqpVhost.value}"
-
-    private val connection: Connection? = try {
-        factory.newConnection(amqpUri).also { connection ->
-            connection.use { usedConnection: Connection ->
-                usedConnection.createChannel().use { channel: com.rabbitmq.client.Channel ->
-
-                    val queueName = configuration.amqpProviderQueuePrefix.value + "-IN"
-
-                    channel.queueDeclare(queueName, false, false, true, emptyMap())
-                    channel.queueBind(
-                        queueName,
-                        configuration.amqpCodecExchangeName.value,
-                        configuration.amqpCodecRoutingKeyIn.value
-                    )
-
-                    channel.basicConsume(
-                        queueName,
-                        false,
-                        configuration.amqpProviderConsumerTag.value,
-                        { _, delivery ->
-                            try {
-                                val decodedBatch = MessageBatch.parseFrom(delivery.body)
-
-                                logger.debug {
-                                    val firstId = decodedBatch.messagesList?.first()?.metadata?.id
-                                    val session = firstId?.connectionId?.sessionAlias
-                                    val direcrion = firstId?.direction?.name
-                                    val firstSeqNum = firstId?.sequence
-                                    val lastSeqNum = decodedBatch.messagesList?.last()?.metadata?.id?.sequence
-                                    val count = decodedBatch.messagesCount
-
-                                    "codec response received (session=$session direction=$direcrion firstSeqNum=$firstSeqNum lastSeqNum=$lastSeqNum count=$count)"
-                                }
-
-                                decodedBatch.messagesList.forEach { message ->
-                                    val match = decodeRequests.firstOrNull { it.first == message.metadata.id }
-
-                                    if (match != null) {
-                                        logger.debug { "response found - ${match.first}" }
-
-                                        match.let { decodeRequests.remove(it) }
-                                        GlobalScope.launch { match.second.send(message) }
-
-                                    } else {
-                                        logger.warn { "decoded message '${message?.metadata?.id}' was received but no request was found" }
-                                    }
-                                }
-
-                                channel.basicAck(delivery.envelope.deliveryTag, false)
-                            } catch (e: InvalidProtocolBufferException) {
-                                logger.error { "unable to parse delivery '${delivery.envelope.deliveryTag}' data:'${delivery.body}'" }
-                            }
-
-                        },
-                        { consumerTag -> logger.error { "consumer '$consumerTag' was unexpectedly cancelled" } }
-                    )
-                }
+    private val connection: Connection? =
+        try {
+            ConnectionFactory().also {
+                it.host = configuration.amqpHost.value
+                it.port = configuration.amqpPort.value.toInt()
+                it.username = configuration.amqpUsername.value
+                it.password = configuration.amqpPassword.value
+                it.virtualHost = configuration.amqpVhost.value
             }
+                .newConnection()
+                .also { connection ->
+                    connection.use { usedConnection: Connection ->
+                        usedConnection.createChannel().use { channel: com.rabbitmq.client.Channel ->
+
+                            val queueName = configuration.amqpProviderQueuePrefix.value + "-IN"
+
+                            channel.queueDeclare(queueName, false, false, true, emptyMap())
+                            channel.queueBind(
+                                queueName,
+                                configuration.amqpCodecExchangeName.value,
+                                configuration.amqpCodecRoutingKeyIn.value
+                            )
+
+                            channel.basicConsume(
+                                queueName,
+                                false,
+                                configuration.amqpProviderConsumerTag.value,
+                                { _, delivery ->
+                                    try {
+                                        val decodedBatch = MessageBatch.parseFrom(delivery.body)
+
+                                        logger.debug {
+                                            val firstId = decodedBatch.messagesList?.first()?.metadata?.id
+                                            val session = firstId?.connectionId?.sessionAlias
+                                            val direcrion = firstId?.direction?.name
+                                            val firstSeqNum = firstId?.sequence
+                                            val lastSeqNum = decodedBatch.messagesList?.last()?.metadata?.id?.sequence
+                                            val count = decodedBatch.messagesCount
+
+                                            "codec response received (session=$session direction=$direcrion firstSeqNum=$firstSeqNum lastSeqNum=$lastSeqNum count=$count)"
+                                        }
+
+                                        decodedBatch.messagesList.forEach { message ->
+                                            val match = decodeRequests.firstOrNull { it.first == message.metadata.id }
+
+                                            if (match != null) {
+                                                logger.debug { "response found - ${match.first}" }
+
+                                                match.let { decodeRequests.remove(it) }
+                                                GlobalScope.launch { match.second.send(message) }
+
+                                            } else {
+                                                logger.warn { "decoded message '${message?.metadata?.id}' was received but no request was found" }
+                                            }
+                                        }
+
+                                        channel.basicAck(delivery.envelope.deliveryTag, false)
+                                    } catch (e: InvalidProtocolBufferException) {
+                                        logger.error { "unable to parse delivery '${delivery.envelope.deliveryTag}' data:'${delivery.body}'" }
+                                    }
+
+                                },
+                                { consumerTag -> logger.error { "consumer '$consumerTag' was unexpectedly cancelled" } }
+                            )
+                        }
+                    }
+                }
+        } catch (e: Exception) {
+            logger.error(e) { "unable to establish amqp connection" }
+            null
         }
-    } catch (e: Exception) {
-        logger.error(e) { "unable to establish amqp connection" }
-        null
-    }
 
     suspend fun decodeMessage(batch: RawMessageBatch): Collection<Message> {
         if (connection == null) {
