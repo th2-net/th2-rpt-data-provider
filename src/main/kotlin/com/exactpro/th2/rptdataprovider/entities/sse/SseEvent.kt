a2a0650915a035d4c17d1e0faaaabac14bcff309
Artem Davidovich <artem.davidovich@exactprosystems.com>
2021-03-16 16:46:24 +0400

    TH2-1629 Fix scanCounter field.

--- src/main/kotlin/com/exactpro/th2/rptdataprovider/entities/sse/SseEvent.kt
+++ src/main/kotlin/com/exactpro/th2/rptdataprovider/entities/sse/SseEvent.kt
@@ -31,7 +31,7 @@ enum class EventType {
     }
 }
 
-data class LastScannedObjectInfo(var id: String = "", var timestamp: Long = 0, var scanCounter: String = "")
+data class LastScannedObjectInfo(var id: String = "", var timestamp: Long = 0, var scanCounter: Long = 0)
 
 data class ExceptionInfo(val exceptionName: String, val exceptionCause: String)
 
