00eaf23c90286a50757300a423f3e3af3e2a65db
Artem Lezgyan <33369719+Paleontolog@users.noreply.github.com>
2022-01-18 17:28:08 +0400

    restore text filters

--- src/main/kotlin/com/exactpro/th2/rptdataprovider/Context.kt
+++ src/main/kotlin/com/exactpro/th2/rptdataprovider/Context.kt
@@ -31,10 +31,12 @@ import com.exactpro.th2.rptdataprovider.entities.filters.events.AttachedMessageF
 import com.exactpro.th2.rptdataprovider.entities.filters.events.EventBodyFilter
 import com.exactpro.th2.rptdataprovider.entities.filters.events.EventNameFilter
 import com.exactpro.th2.rptdataprovider.entities.filters.events.EventStatusFilter
+import com.exactpro.th2.rptdataprovider.entities.filters.events.EventTextFilter
 import com.exactpro.th2.rptdataprovider.entities.filters.events.EventTypeFilter
 import com.exactpro.th2.rptdataprovider.entities.filters.messages.AttachedEventFilters
 import com.exactpro.th2.rptdataprovider.entities.filters.messages.MessageBodyBinaryFilter
 import com.exactpro.th2.rptdataprovider.entities.filters.messages.MessageBodyFilter
+import com.exactpro.th2.rptdataprovider.entities.filters.messages.MessageTextFilter
 import com.exactpro.th2.rptdataprovider.entities.filters.messages.MessageTypeFilter
 import com.exactpro.th2.rptdataprovider.entities.internal.MessageWithMetadata
 import com.exactpro.th2.rptdataprovider.entities.responses.BaseEventEntity
@@ -98,6 +100,7 @@ class Context(
             AttachedMessageFilter.filterInfo to AttachedMessageFilter.Companion::build,
             EventTypeFilter.filterInfo to EventTypeFilter.Companion::build,
             EventNameFilter.filterInfo to EventNameFilter.Companion::build,
+            EventTextFilter.filterInfo to EventTextFilter.Companion::build,
             EventBodyFilter.filterInfo to EventBodyFilter.Companion::build,
             EventStatusFilter.filterInfo to EventStatusFilter.Companion::build
         ), cradleService
@@ -107,6 +110,7 @@ class Context(
         mapOf(
             AttachedEventFilters.filterInfo to AttachedEventFilters.Companion::build,
             MessageTypeFilter.filterInfo to MessageTypeFilter.Companion::build,
+            MessageTextFilter.filterInfo to MessageTextFilter.Companion::build,
             MessageBodyFilter.filterInfo to MessageBodyFilter.Companion::build,
             MessageBodyBinaryFilter.filterInfo to MessageBodyBinaryFilter.Companion::build
         ), cradleService
