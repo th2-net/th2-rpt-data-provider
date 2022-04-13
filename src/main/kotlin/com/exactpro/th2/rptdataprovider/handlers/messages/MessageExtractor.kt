6e2fe40b1ee3a6a1374afd1773fa1a434eb97c21
Artem Davidovich <artem.davidovich@exactprosystems.com>
2022-04-13 16:26:58 +0400

    Added new metrics.

--- src/main/kotlin/com/exactpro/th2/rptdataprovider/handlers/messages/MessageExtractor.kt
+++ src/main/kotlin/com/exactpro/th2/rptdataprovider/handlers/messages/MessageExtractor.kt
@@ -20,13 +20,10 @@ import com.exactpro.cradle.Order
 import com.exactpro.cradle.TimeRelation
 import com.exactpro.cradle.messages.StoredMessageFilterBuilder
 import com.exactpro.cradle.messages.StoredMessageId
-import com.exactpro.th2.common.grpc.RawMessage
 import com.exactpro.th2.rptdataprovider.Context
 import com.exactpro.th2.rptdataprovider.entities.internal.EmptyPipelineObject
 import com.exactpro.th2.rptdataprovider.entities.internal.PipelineRawBatch
 import com.exactpro.th2.rptdataprovider.entities.requests.SseMessageSearchRequest
-import com.exactpro.th2.rptdataprovider.entities.responses.MessageBatchWrapper
-import com.exactpro.th2.rptdataprovider.entities.responses.MessageWrapper
 import com.exactpro.th2.rptdataprovider.entities.responses.StoredMessageBatchWrapper
 import com.exactpro.th2.rptdataprovider.entities.sse.StreamWriter
 import com.exactpro.th2.rptdataprovider.handlers.PipelineComponent
@@ -223,6 +220,8 @@ class MessageExtractor(
                         pipelineStatus.countFetchedBytes(streamName.toString(), batch.batchSize)
                         pipelineStatus.countFetchedBatches(streamName.toString())
                         pipelineStatus.countFetchedMessages(streamName.toString(), trimmedMessages.size.toLong())
+                        pipelineStatus.countSkippedMessages(streamName.toString(), batch.messageCount - trimmedMessages.size.toLong())
+                        pipelineStatus.countSkippedBatches(streamName.toString(), if (trimmedMessages.isEmpty()) 1 else 0 )
                     } else {
                         logger.trace { "exiting $streamName loop" }
                         break
