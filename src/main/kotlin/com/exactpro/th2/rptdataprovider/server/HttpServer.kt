90a420221e0d9f1bdd48f8c9aeb0daee82ca7e35
Oleg <oleg.smirnov@exactprosystems.com>
2022-12-21 11:17:27 +0400

    [TH2-4563] Install IgnoreTrailingSlash plugin

--- src/main/kotlin/com/exactpro/th2/rptdataprovider/server/HttpServer.kt
+++ src/main/kotlin/com/exactpro/th2/rptdataprovider/server/HttpServer.kt
@@ -314,6 +314,7 @@ class HttpServer(private val applicationContext: Context) {
             install(Timeouts) {
                 requestTimeout = applicationContext.timeout
             }
+            install(IgnoreTrailingSlash)
 
             routing {
 
